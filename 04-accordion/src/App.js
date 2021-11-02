384f1b94a6082949d49fdd642a18c7b1fc5812df
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2021-11-03 04:32:05 +0530

    change title of all prev project

---
+++ 04-accordion/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import data from './data';
+import Question from './Question';
+import SingleQuestion from './Question';
+function App() {
+  return (
+    <main>
+      <div
+        className="container">
+        
+      <Question/>
+      </div>
+    </main>
+  )
+}
+
+export default App;
