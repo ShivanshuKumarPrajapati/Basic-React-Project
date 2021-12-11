8202daf12d15d4f330aff2e347c4b56434ca86ab
ShivanshuKumarPrajapati <shivanshu.rkt@gmail.com>
2021-12-11 22:44:17 +0530

    complete grocery-bud project

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -3,15 +3,25 @@ import List from './List'
 import Alert from './Alert'
 import { FaTimesCircle } from 'react-icons/fa';
 
+const getlocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'));
+  }
+  else
+    return [];
+}
+
 function App() {
   const [item, setItem] = useState('');
   const [placeHolder, setPlaceHolder] = useState("eg. egg");
   const [alertMssg, setAlertMssg] = useState('');
   const [alertClass, setAlertClass] = useState('');
   const [alert, setAlert] = useState(false);
-  const [list, setList] = useState([]);
-  // const [submitAlert, setSubmitAlert] = useState(false);
-
+  const [list, setList] = useState(getlocalStorage());
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState('')
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     if (item === '')
@@ -19,6 +29,20 @@ function App() {
       setAlert(true);
       setAlertClass('alert-danger');
       setAlertMssg("Please Enter Value");
+    }
+    else if (item && edit)
+    {
+      list.map((element) => {
+        if (element.id === id)
+        {
+          element.title = item;
+          }
+      })
+      setAlert(true);
+      setAlertMssg("Saved Changes");
+      setAlertClass("alert-success");
+      setId('');
+      setEdit(false);
       }
     else
     {
@@ -32,6 +56,24 @@ function App() {
     setItem('');
     setPlaceHolder('eg. egg');
   }
+  const removeItem = (id) => {
+    setAlert(true);
+    setAlertClass('alert-danger');
+    setAlertMssg('Item removed');
+    setList(list.filter((item) => item.id != id));
+  }
+
+  const handleClear = () => {
+    setAlert(true);
+    setAlertClass('alert-danger');
+    setAlertMssg('Empty List');
+    setList([]);
+  }
+  const EditItem = (item) => {
+    setItem(item.title);
+    setEdit(true);
+    setId(item.id);
+  }
   
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -40,9 +82,13 @@ function App() {
     return () => clearTimeout(timeout);
   }, [alert]);
 
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  },[list])
+
   return (
     <div className="section-center">
-      {alert ? <Alert class={alertClass} value={alertMssg} />:''}
+      {alert ? <Alert class={alertClass} value={alertMssg} /> : ""}
       <form className="grocery-form">
         <h3>Grocery Bud</h3>
         <div className="form-control">
@@ -54,11 +100,21 @@ function App() {
             onChange={(e) => setItem(e.target.value)}
           />
           <button className="submit-btn" onClick={handleSubmit}>
-            Submit
+            {edit ? `Save` : `Submit`}
           </button>
         </div>
       </form>
-      {list.length ? <List items={list}></List> : ''}
+
+      {list.length ? (
+        <div className="grocery-container">
+          <List items={list} removeItem={removeItem} EditItem={EditItem}></List>{" "}
+          <button className="clear-btn" onClick={handleClear}>
+            Clear Item
+          </button>{" "}
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
--- 10-grocery-bud/src/List.js
+++ 10-grocery-bud/src/List.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({ items }) => {
-  console.log(items);
-  return (
-  <div className='grocery-container'>
-      {
-        items.map((item) => {
-          return (
-          <div className="grocery-item" key={item.id}>
-              <p className='title'>{item.title}</p>
-              <div className="btn-container">
-            <FaEdit className="edit-btn" />
-            <FaTrash className="delete-btn"/>
-              </div>
-          </div>
-          )
-        })
-        }
-    <button className="clear-btn" >Clear Item</button>
-  </div>
-  )
+const List = ({ items,removeItem,EditItem}) => {
+
+  return items.map(( item) => {
+  
+    return (
+      <div className="grocery-item" key={item.id}>
+        <p className="title">{item.title}</p>
+        <div className="btn-container">
+          <FaEdit className="edit-btn" onClick={()=>EditItem(item)}/>
+          <FaTrash className="delete-btn" onClick={() => removeItem(item.id)} />
+        </div>
+      </div>
+    );
+  });
 }
 
 export default List
